c134c6198745cb429249765db4324d2927342bda
Levi-RailsContainer <ljgreb2001@gmail.com>
2022-10-22 22:07:18 +0000

    defined the sum and max_2_sum functions

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,11 +3,32 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  #initalize result to -1
+  result = -1 
+  #if the array is empty return 0
+  if arr.empty?
+    result = 0
+  #otherwise return the sum of all the array's elements
+  else
+    result = arr.sum
+  return result
+  end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  #initalize result to -1
+  result = -1
+  #if the array is empty return 0
+  if arr.empty?
+    result = 0
+  #if array contains only one element, return that element
+  elsif arr.count == 1
+    result = arr.first
+  #othewise return the top 2 maximum values added together
+  else
+    result = arr.max(2).reduce(:+)
+  end
+  return result
 end
 
 def sum_to_n? arr, n
