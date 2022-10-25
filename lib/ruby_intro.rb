6d3bc0bd960dedc83654177f07138ce0f1ccaaf8
Levi-RailsContainer <ljgreb2001@gmail.com>
2022-10-25 19:23:22 +0000

    fixed comment issues for sum_to_n?

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -32,15 +32,14 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  #initalize result to
+  #initalize result to false
   result = false
-  #if the array is empty return 0
+  #if the array is empty display error message, and keep result as false
   if arr.empty?
     puts "Error: array is empty."#result = 0
   #if the array contains two elements that sum to n, return true
   else arr.combination(2).to_a.select { |a, b| n == a + b }.each do
     result = true; end
-#otherwise return false
   end
   return result
 end
